80af14160b4a086bfac78c92824b6e49786c44b1
wnorris347 <wnorris347@gmail.com>
2023-03-11 21:57:57 +0000

    Updates

--- Midterm/index.js
+++ Midterm/index.js
@@ -1,26 +1,48 @@
-pullRegGoods();
-
-function pullRegGoods(){
-    fetch("./data.json")
-        .then(function (response){
-            return response.json;
-        }).then(function (data){
-            appendRegData(data);
-        }).catch(function (error){
-            console.log("error: " + error);
-        })
-}
+/*fetch("./data.json")
+            .then(function (response){
+                return response.json
+            }).then(function (data){
+                appendRegData(data);
+            }).catch(function (err){
+                console.log('error: ' +err);
+            })
 
 function appendRegData(data){
-    let containers = document.getElementsByClassName("bakedGood");
-    let i = 0;
-    for (let element of data['Baked Goods']) {
-        let mainContainer = containers[i];
+    console.log(data);
+    data = JSON.parse(data);
+    console.log(data);
+    for (let element of data) {
         console.log(element);
-        let div2 = document.createElement("div");
-        div2.innerHTML = `${element["productName"]} <br> ${element["price"]} <br> ${element["description"]}`;
-        mainContainer.appendChild(div2);
-        i++;
+        for (product of data[element]){
+            if(document.getElementById(product["productName"]) != null){
+                let mainContainer = document.getElementById(product["productName"]);
+                let div2 = document.createElement("div");
+                div2.innerHTML = `${product["productName"]} <br> $${product["price"]} <br> ${product["description"]}`;
+                mainContainer.appendChild(div2);
+            }
+        }
     }
-}
+}*/
 
+fetch("./data.json")
+            .then(function (response){
+                return response.json();
+            }).then(function (data){
+                appendData(data);
+            }).catch(function (err){
+                console.log('error: ' +err);
+            })
+
+        function appendData(data){
+            for (let productName in data){
+                for (let element of data[productName]) {
+                    if(document.getElementById(element["productName"]) != null){
+                        let mainContainer = document.getElementById(element["productName"]);
+                        console.log(element);
+                        let div2 = document.createElement("div");
+                        div2.innerHTML = `${element["productName"]}<br>$${element["price"]} <br>${element["description"]}`;
+                        mainContainer.appendChild(div2);
+                    }
+                }
+            }
+        }
