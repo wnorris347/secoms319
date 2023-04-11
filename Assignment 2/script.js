7da8b09a634ba1f2be68c3c9b8d8c6b2291bc140
wnorris347 <wnorris347@gmail.com>
2023-04-11 18:15:27 -0500

    update

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,4 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import React, {useState, useEffect} from "react";
+import items from "./data.json";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const Shop = () => {
+    const [cart, setCart] = useState([]);
+    const [cartTotal, setCartTotal] = useState(0);
+
+    return(
+        <div>Hello</div>
+    );
+
+    const addToCart = (el) => {
+        setCart([...cart, el]);
+    }
+
+    const removeFromCart = (el) => {
+        let hardCopy = [...cart];
+        hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
+        setCart(hardCopy);
+    }
+
+    const listItems = items.map((el) => (
+        <div key={el.id}>
+            <img class="img-fluid" src={el.image} />
+            {el.title}
+            {el.category}
+            {el.price}
+            <button type="button" onClick={() => removeFromCart(el)}>-</button>{" "}
+            <button type="button" variant="light" onClick={() => addToCart(el)}>+</button>
+        </div>
+    ));
+
+    return(
+        <div>
+            {listItems}
+        </div>
+    );
+}
+
+export default Shop;
