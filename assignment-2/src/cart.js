1d8d71bcd138726895b84a088980c476ee14443b
wnorris347 <wnorris347@gmail.com>
2023-04-12 18:19:56 -0500

    update

--- assignment-2/src/cart.js
+++ assignment-2/src/cart.js
@@ -10,47 +10,29 @@ function Cart(){
     const [name, setName] = useState('');
     const [card, setCard] = useState('');
     const [expire, setExpire] = useState('');
-    const [cv, setCv] = useState('');
+    const [cv, setCV] = useState('');
+    const [email, setEmail] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zip, setZip] = useState('');
+    const [address, setAddress] = useState('');
+    const [address2, setAddress2] = useState('');
 
-    const handleName = 
 
+    const handleName = (e) => {
+        const newName = e.target.value;
+        setName(newName);
+    };
 
-    const printCart = () => {
-        <div>
-            Bakery Bros
-                <div class="row">
-                    <div class="col-md-8 cart">
-                        <div class="title">
-                            <div class="row">
-                                <div class="col">
-                                    <h4>
-                                        Cart
-                                    </h4>
-                                </div>
-                                <div class="col align-self-center text-right text-muted">
-                                    Products selected: {cart.length}
-                                </div>
-                            </div>
-                        </div>
-                        <div>{listItems}</div>
-                    </div>
-                    <div class="float-end">
-                        <p class="mb-0 me-5 d-flex align-items-center">
-                            <span class="small text-muted me-2">Order subtotal: </span>
-                            <span class="lead fw-normal">${cartTotal}</span>
-                        </p>
-                        <p class="mb-0 me-5 d-flex align-items-center">
-                            <span class="small text-muted me-2">Sales tax: </span>
-                            <span class="lead fw-normal">${(cartTotal * 0.07).toFixed(2)}</span>
-                        </p>
-                        <p class="mb-0 me-5 d-flex align-items-center">
-                            <span class="small text-muted me-2">Order total: </span>
-                            <span class="lead fw-normal">${(cartTotal * 1.07).toFixed(2)}</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
+    const handleCard = (e) => {
+        const newCard = e.target.value;
+        setCard(newCard);
     }
 
+    const handleExpire = (e) => {
+        const newExpire = e.target.value;
+        setExpire(newExpire);
+    }
 
+    
 }
