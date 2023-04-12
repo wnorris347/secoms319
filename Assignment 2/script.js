ba16f11dfcb4d7be4f433ebd6dc6d72a50586341
kylenach <kyleq00@gmail.com>
2023-04-11 19:02:04 -0500

    update

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import React, {useState, useEffect} from "react";
 import items from "./data.json";
+import 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
