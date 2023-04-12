e6f587185e27b8d992fbb68c7a4d668cd448546a
wnorris347 <wnorris347@gmail.com>
2023-04-11 23:10:33 -0500

    update

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -48,23 +48,17 @@ const Shop = () => {
                         <div key={index} classNameclassName="group relative shadow-lg" >
                             <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
                                 <img
-                                    alt="Product Image"
+                                    alt={product.description}
                                     src={product.image}
                                     className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                                 />
                             </div>
                             <div className="flex justify-between p-3">
-                                <div>
-                                    <h3 className="text-sm text-gray-700">
-                                        <a href={product.href}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
-                                            <span style={{ fontSize: '16px', fontWeight: '600' }}>{product.title}</span>
-                                        </a>
-                                        <p>Tag - {product.category}</p>
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-500">Rating: {product.rating.rate}</p>
-                                </div>
                                 <p className="text-sm font-medium text-green-600">${product.price}</p>
+                            </div>
+                            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                <button type="button" onClick={() => removeFromCart(product)}>-</button>{" "}
+                                <button type="button" variant="light" onClick={() => addToCart(product)}>+</button>
                             </div>
                         </div>
                     ))}
