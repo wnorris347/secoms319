f59c6f6a630a9646733573bf77cbb12b5a4b8b71
wnorris347 <wnorris347@gmail.com>
2023-04-12 21:38:15 -0500

    update

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -1,1 +1,23 @@
 import React, {useState, useEffect} from "react";
+
+const Shop = () => {
+    const [cart, setCart] = useState([]);
+    const [cartTotal, setCartTotal] = useState(0);
+    const [ProductsCategory, setProductsCategory] = useState(items);
+    const [query, setQuery] = useState("");
+    const [show, setShow] = useState(false);
+
+    const hideModal = () => setShow(false);
+    const showModal = () => setShow(true);
+
+    const [name, setName] = useState('');
+    const [card, setCard] = useState('');
+    const [expire, setExpire] = useState('');
+    const [cv, setCV] = useState('');
+    const [email, setEmail] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zip, setZip] = useState('');
+    const [address, setAddress] = useState('');
+    const [address2, setAddress2] = useState('');
+}
