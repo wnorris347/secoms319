4af92e1114b70cbcaa903c70d570664cf4f6278e
wnorris347 <wnorris347@gmail.com>
2023-04-30 17:59:40 -0500

    update

---
+++ Assignment-3/mern_backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+let Product = require('./dataSchema.js');
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static("public"));
+app.use("/images", express.static("imaages"));
+
+mongoose.connect("mongodb://localhost:27017/reactdata",
+    {
+        dbName: "reactdata",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+const port = process.env.PORT || 4000;
+const host = "localhost";
+app.listen(port, () => {
+    console.log(`App listening at http://%s:%s`, host, port);
+});
+
+app.get("/", async (req, resp) => {
+    const query = {};
+    const allProducts = await Product.find(query);
+    console.log(allProducts);
+    resp.send(allProducts);
+});
