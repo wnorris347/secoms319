c534febe8a67189fc9ee77e02fc2c391ad42c5c8
wnorris347 <wnorris347@gmail.com>
2023-05-01 13:33:23 -0500

    update

--- Assignment-3/mern-frontend/mern_frontend/src/App.js
+++ Assignment-3/mern-frontend/mern_frontend/src/App.js
@@ -7,6 +7,20 @@ function App() {
   const [oneProduct, setOneProduct] = useState([]);
   const [viewer2, setViewer2] = useState(false);
 
+  const [addNewProduct, setAddNewProduct] = useState({
+    _id: 0,
+    title: "",
+    price: 0.0,
+    description: "",
+    category: "",
+    image: "http://127.0.0.1:4000/images/",
+    rating: { rate: 0.0, count: 0 },
+  });
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   function getAllProducts() {
     fetch("http://localhost:4000/")
       .then((response) => response.json())
@@ -55,7 +69,53 @@ function App() {
       Rate :{el.rating.rate} and Count:{el.rating.count} <br />
     </div>
   ));
-    
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    if (evt.target.name === "_id") {
+      setAddNewProduct({ ...addNewProduct, _id: value });
+    } else if (evt.target.name === "title") {
+      setAddNewProduct({ ...addNewProduct, title: value });
+    } else if (evt.target.name === "price") {
+      setAddNewProduct({ ...addNewProduct, price: value });
+    } else if (evt.target.name === "description") {
+      setAddNewProduct({ ...addNewProduct, description: value });
+    } else if (evt.target.name === "category") {
+      setAddNewProduct({ ...addNewProduct, category: value });
+    } else if (evt.target.name === "image") {
+    const temp = value;
+      setAddNewProduct({ ...addNewProduct, image: temp });
+    } else if (evt.target.name === "rate") {
+      setAddNewProduct({ ...addNewProduct, rating: { rate: value } });
+    } else if (evt.target.name === "count") {
+      const temp = addNewProduct.rating.rate;
+      setAddNewProduct({
+        ...addNewProduct,
+        rating: { rate: temp, count: value },
+      });
+    }
+  }
+
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    fetch("http://localhost:4000/insert", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(addNewProduct),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Post a new product completed");
+        console.log(data);
+        if (data) {
+          //const keys = Object.keys(data);
+          const value = Object.values(data);
+          alert(value);
+        }
+      });
+  }  
     
 
 return (
@@ -72,6 +132,22 @@ return (
     <h1>Show one Product by Id:</h1>
     {viewer2 && <div>Product: {showOneItem}</div>}
     <hr></hr>
+    <div>
+      <h3>Add a new product :</h3>
+      <form action="">
+        <input type="number" placeholder="id?" name="_id" value={addNewProduct._id} onChange={handleChange} />
+        <input type="text" placeholder="title?" name="title" value={addNewProduct.title} onChange={handleChange} />
+        <input type="number" placeholder="price?" name="price" value={addNewProduct.price} onChange={handleChange} />
+        <input type="text" placeholder="description?" name="description" value={addNewProduct.description} onChange={handleChange} />
+        <input type="text" placeholder="category?" name="category" value={addNewProduct.category} onChange={handleChange} />
+        <input type="text" placeholder="image?" name="image" value={addNewProduct.image} onChange={handleChange} />
+        <input type="number" placeholder="rate?" name="rate" value={addNewProduct.rating.rate} onChange={handleChange} />
+        <input type="number" placeholder="count?" name="count" value={addNewProduct.rating.count} onChange={handleChange} />
+        <button type="submit" onClick={handleOnSubmit}>
+          submit
+        </button>
+      </form>
+    </div>
   </div>
 )
 }
--- Assignment-3/mern_backend/index.js
+++ Assignment-3/mern_backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-let Product = require('./dataSchema.js');
+const Product = require("./dataSchema.js");
 
 app.use(express.json());
 app.use(cors());
@@ -10,16 +10,15 @@ app.use(cors());
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
 
-mongoose.connect("mongodb://localhost:27017/reactdata",
-    {
-        dbName: "reactdata",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-);
+mongoose.connect("mongodb://127.0.0.1:27017/reactdata", {
+    dbName: "reactdata",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 const port = process.env.PORT || 4000;
 const host = "localhost";
+
 app.listen(port, () => {
     console.log(`App listening at http://%s:%s`, host, port);
 });
