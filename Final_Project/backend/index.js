fb7f7fee638485ae436ab5a68228717fc8958004
wnorris347 <wnorris347@gmail.com>
2023-05-03 20:22:48 -0500

    update

--- Final_Project/backend/index.js
+++ Final_Project/backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-let Product = require('./dataSchema.js');
+let Product = require('./DataSchema.js');
 
 app.use(express.json());
 app.use(cors());S
