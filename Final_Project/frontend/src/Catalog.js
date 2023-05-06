e73d73ad57c8dc599cd7ad9b1509295028a13a12
kylenach <kyleq00@gmail.com>
2023-05-06 16:21:46 -0500

    a

--- Final_Project/frontend/src/Catalog.js
+++ Final_Project/frontend/src/Catalog.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import './custom.scss';
 
 const Shop = () => {
     const [cart, setCart] = useState([]);
