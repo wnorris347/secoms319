fe6928d1548d74f4b6d876211c12d9493b778c42
kylenach <kyleq00@gmail.com>
2023-05-06 15:00:26 -0500

    sync

--- Final_Project/frontend/src/index.js
+++ Final_Project/frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css';
+import './custom.scss';
 import App from './App';
+// import "https://getbootstrap.com/docs/5.3/examples/album/"
+// import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
